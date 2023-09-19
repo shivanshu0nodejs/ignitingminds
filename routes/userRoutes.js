f502508095c261e85829371246871e86fd128e48
shivanshu0nodejs <145346118+shivanshu0nodejs@users.noreply.github.com>
2023-09-19 17:23:41 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const {home} = require("../controllers/homeController.js");
-const {signup, login, otpVerification, loginOtp} = require("../controllers/userController.js");
+const {signup, login, otpVerification, loginOtp, resendOtp} = require("../controllers/userController.js");
 
 
 router.route("/").get(home);
@@ -11,6 +11,8 @@ router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/otpverification").post(otpVerification);
 router.route("/loginotp").post(loginOtp);
+router.route("/resendotp").post(resendOtp);
+
 
 module.exports = router;
 
